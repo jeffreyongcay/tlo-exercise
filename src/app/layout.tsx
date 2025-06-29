07dac1d024860e34f9c11bd5058d24d83b2d1c88
Jeffrey Ongcay <jeffreyongcay@gmail.com>
2025-06-29 20:45:37 +0800

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Rubik } from "next/font/google";
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  variable: "--font-rubik",
+  weight: ["400", "500", "600", "700"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${rubik.variable} antialiased`}
       >
         {children}
       </body>
---
+++ src/components/custom-ui/card-item.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  data: ProductData;
+}
+
+const CardItem: React.FC<Props> = ({ data }) => {
+  return (
+    <div className="relative rounded border border-[#e6e6e6] p-4 bg-white space-y-3">
+      <div>
+        <img src={data.imageUrl} />
+      </div>
+      <div className="space-y-1">
+        <h3 className="text-sm font-rubik">{data.name}</h3>
+        <div>
+          <span className="text font-rubik font-bold">${data.price}</span>
+          <span className="text-sm text-red-500 ml-2 line-through">$24.95</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
